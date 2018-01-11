623e6faf469c1cc90da6862f73130239bd1283d6
rgreen88 <rynelgreen@gmail.com>
2018-01-11 13:39:22 -0500

    finished Presenter, creating Firebase model

---
+++ app/src/main/java/com/example/ryne/jpmclookalikemvp/model/FirebaseDbModel.java
@@ -0,0 +1,8 @@
+package com.example.ryne.jpmclookalikemvp.model;
+
+/**
+ * Created by Ryne on 1/11/2018.
+ */
+
+public class FirebaseDbModel {
+}
--- app/src/main/java/com/example/ryne/jpmclookalikemvp/presenter/MainPresenter.java
+++ app/src/main/java/com/example/ryne/jpmclookalikemvp/presenter/MainPresenter.java
@@ -1,8 +1,21 @@
 package com.example.ryne.jpmclookalikemvp.presenter;
 
+import com.example.ryne.jpmclookalikemvp.view.MainContract;
+
 /**
  * Created by Ryne on 1/11/2018.
  */
 
-public class MainPresenter {
+public class MainPresenter implements MainContract.Presenter{
+
+    MainContract.View mView;
+
+    public MainPresenter (MainContract.View mView){
+        this.mView = mView; //binding relationship from contract to presenter
+    }
+
+    @Override
+    public void getInstance() {
+
+    }
 }

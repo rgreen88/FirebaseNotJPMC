6d39053b0d133da071d561a6923c27b1f343b265
rgreen88 <rynelgreen@gmail.com>
2018-01-11 17:33:35 -0500

    created FirebaseAuthHandler and CipherHandler classes. constructor in place for Cipher

---
+++ app/src/main/java/com/example/ryne/jpmclookalikemvp/model/util/CipherHandler.java
@@ -0,0 +1,18 @@
+package com.example.ryne.jpmclookalikemvp.model.util;
+
+import java.security.Provider;
+
+import javax.crypto.CipherSpi;
+
+/**
+ * Created by Ryne on 1/11/2018.
+ */
+
+public class CipherHandler {
+
+    //Cipher constructor to create cipher object
+    public CipherHandler(CipherSpi cipherSpi, Provider provider, String transformation){
+
+
+    }
+}

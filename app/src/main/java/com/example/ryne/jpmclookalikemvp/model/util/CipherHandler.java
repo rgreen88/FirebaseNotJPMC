c01d937586bb5d28703e355383c05c4251328a0d
rgreen88 <rynelgreen@gmail.com>
2018-01-12 20:26:37 -0500

    added encrypt method to CipherHandler

--- app/src/main/java/com/example/ryne/jpmclookalikemvp/model/util/CipherHandler.java
+++ app/src/main/java/com/example/ryne/jpmclookalikemvp/model/util/CipherHandler.java
@@ -1,8 +1,19 @@
 package com.example.ryne.jpmclookalikemvp.model.util;
 
+
+import android.util.Base64;
+
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 import java.security.Provider;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
 import javax.crypto.CipherSpi;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
 
 /**
  * Created by Ryne on 1/11/2018.
@@ -10,9 +21,29 @@ import javax.crypto.CipherSpi;
 
 public class CipherHandler {
 
-    //Cipher constructor to create cipher object
-    public CipherHandler(CipherSpi cipherSpi, Provider provider, String transformation){
+    private final Cipher cipher;
+    private String transformation;
 
+    //Cipher constructor
+    public CipherHandler(String transformation) throws NoSuchPaddingException, NoSuchAlgorithmException{
+
+        this.transformation = transformation;
+
+        cipher = Cipher.getInstance(transformation);
+    }
+
+    //encrypt method
+    public String encrypt(String data, Key key) throws InvalidKeyException, BadPaddingException, IllegalBlockSizeException{
+
+        //initiating encrypt mode for key
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+
+        //creating bytes array and encrypting data using .getBytes method
+        byte[] bytes = cipher.doFinal(data.getBytes());
+
+        //returning encryption data using Base64 default
+        return Base64.encodeToString(bytes, Base64.DEFAULT);
 
     }
+
 }

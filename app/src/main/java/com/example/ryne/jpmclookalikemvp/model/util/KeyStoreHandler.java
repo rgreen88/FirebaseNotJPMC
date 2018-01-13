dae33f88a0d2e97100705c6791186f4414e62b0a
rgreen88 <rynelgreen@gmail.com>
2018-01-12 21:23:37 -0500

    created KeyGenParamSpec targetting newer devices.  Added boolean to return newer ver os

--- app/src/main/java/com/example/ryne/jpmclookalikemvp/model/util/KeyStoreHandler.java
+++ app/src/main/java/com/example/ryne/jpmclookalikemvp/model/util/KeyStoreHandler.java
@@ -1,11 +1,16 @@
 package com.example.ryne.jpmclookalikemvp.model.util;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
+import android.security.KeyPairGeneratorSpec;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
 import android.support.annotation.RequiresApi;
 import android.util.Log;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -15,6 +20,8 @@ import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.cert.CertificateException;
 import java.util.Calendar;
+
+import javax.security.auth.x500.X500Principal;
 
 /**
  * Created by Ryne on 1/12/2018.
@@ -66,8 +73,36 @@ public class KeyStoreHandler {
         //adding date to have first two digits to start with 20 to indicate 2000
         endDate.add(Calendar.YEAR, 20);
 
-        //KeyPairGeneratorSpec deprecated in api 23 --- suggested to use KeyGenParameterSpec
+        //KeyPairGeneratorSpec deprecated in api 23 --- using KeyGenParameterSpec in KeyGen method
+        KeyPairGeneratorSpec keyPairSpec = new KeyPairGeneratorSpec.Builder(context)
+                .setAlias(alias)
+                .setSerialNumber(BigInteger.ONE)
+                .setSubject(new X500Principal("CN=${alias} CA Certificate"))
+                .setStartDate(startDate.getTime())
+                .setEndDate(endDate.getTime())
+                .build();
 
+        keyPairGenerator.initialize(keyPairSpec);
     }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void initGeneratorWithKeyGenParamSpec(KeyPairGenerator keyPairGenerator, String alias) throws InvalidAlgorithmParameterException {
+
+        Log.d(TAG, "initGeneratorWithKeyGenParamSpec: ");
+
+        //KeyGenParameterSpec for newer devices being targeted
+        KeyGenParameterSpec keyGenSpec = new KeyGenParameterSpec.Builder(alias, KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+                .setBlockModes(KeyProperties.BLOCK_MODE_ECB)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1)
+                .build();
+
+        keyPairGenerator.initialize(keyGenSpec);
+    }
+
+    public boolean hasMarshMellow(){
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
+    }
+
+//    public KeyPair
 
 }

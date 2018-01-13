759074baee433c53e7817b126fbe25de0dfad76f
rgreen88 <rynelgreen@gmail.com>
2018-01-12 22:27:38 -0500

    added createAccount method. need sign-in method

--- app/src/main/java/com/example/ryne/jpmclookalikemvp/MainActivity.java
+++ app/src/main/java/com/example/ryne/jpmclookalikemvp/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.ryne.jpmclookalikemvp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.ryne.jpmclookalikemvp.model.util.FirebaseAuthHandler;
 import com.example.ryne.jpmclookalikemvp.presenter.MainPresenter;
 import com.example.ryne.jpmclookalikemvp.view.MainContract;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity implements MainContract.View {
 
+    private static final String TAG = "log";
     MainPresenter presenter;
     private FirebaseAuth mAuth;
 
@@ -28,6 +35,10 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
     @Override
     protected void onStart(){
         super.onStart();
+
+        //checks if user is currently signed on
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        updateUI(currentUser);
         presenter.getInstance();
     }
 
@@ -36,6 +47,29 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
     @Override
     public void showInstance() {
         Toast.makeText(this, "Reading/Writing", Toast.LENGTH_SHORT).show();
+    }
+
+    private void createAccount(){
+//        mAuth.createUserWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            // Sign in success, update UI with the signed-in user's information
+//                            Log.d(TAG, "createUserWithEmail:success");
+//                            FirebaseUser user = mAuth.getCurrentUser();
+//                            updateUI(user);
+//                        } else {
+//                            // If sign in fails, display a message to the user.
+//                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+//                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+//                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+//                        }
+//
+//                        // ...
+//                    }
+//                });
     }
 }
 

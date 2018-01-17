2166ebde980ac2b0c174fa2ac469943cadca341d
rgreen88 <rynelgreen@gmail.com>
2018-01-17 14:30:21 -0500

    added fragments to initialize activities. adding a nav header now

--- app/src/main/java/com/example/ryne/jpmclookalikemvp/view/EmailPasswordActivity.java
+++ app/src/main/java/com/example/ryne/jpmclookalikemvp/view/EmailPasswordActivity.java
@@ -1,9 +1,11 @@
 package com.example.ryne.jpmclookalikemvp.view;
 
+import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -55,6 +57,11 @@ public class EmailPasswordActivity extends CheckingAccountActivity implements Vi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emailpassword);
 
+        // Find our drawer view
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
+        // Setup drawer view
+        setupDrawerContent(nvDrawer);
+
         // Set a Toolbar to replace the ActionBar.
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -92,6 +99,52 @@ public class EmailPasswordActivity extends CheckingAccountActivity implements Vi
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.nav_checking_fragment:
+                fragmentClass = CheckingAccountActivity.class;
+                break;
+            case R.id.nav_transactions_fragment:
+                fragmentClass = TransactionsActivity.class;
+                break;
+            case R.id.nav_market_fragment:
+                fragmentClass = MarketActivity.class;
+                break;
+            default:
+                fragmentClass = CheckingAccountActivity.class;
+        }
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        mDrawer.closeDrawers();
     }
 
     //Start checking user
